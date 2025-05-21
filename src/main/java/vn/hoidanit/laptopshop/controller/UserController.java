fe8b674c9ee1eefd990e9f1fa916daf481c24a0d
Tín <Trungtin0701200@gmail.com>
2025-05-21 23:31:07 +0700

    html form

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,12 +1,15 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,11 +26,25 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello(); //controller gọi từ model (service) để tính toán hàm, dữ liệu
+        String test = this.userService.handleHello(); // controller gọi từ model (service) để tính toán hàm, dữ liệu
         model.addAttribute("eric", test); // model sau khi có data rồi nó sẽ gửi lại thằng controller
         model.addAttribute("tindao", "from controller with model");
         return "hello"; // controller xử lí data và gửi lại cho view
     } // view render ra html
+
+    @RequestMapping("/admin/user") // get
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("newUser", new User() );
+        model.addAttribute("tindao", "from controller with model");
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User Daotrungtin) {
+        System.out.println("run here" + Daotrungtin);
+        return "hello";
+    }
 }
 
 // @RestController

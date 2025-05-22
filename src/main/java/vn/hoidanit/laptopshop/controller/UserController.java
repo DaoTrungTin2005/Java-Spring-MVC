06f94f51908478947f5149d253ba691434c669c1
Tín <Trungtin0701200@gmail.com>
2025-05-22 17:55:30 +0700

    jpa

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,31 +19,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 
     // DI : Dependency injection
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+
     }
+
+    // @RequestMapping("/")
+    // public String getHomePage(Model model) {
+    // String test = this.userService.handleHello(); // controller gọi từ model
+    // (service) để tính toán hàm, dữ liệu
+    // model.addAttribute("eric", test); // model sau khi có data rồi nó sẽ gửi lại
+    // thằng controller
+    // model.addAttribute("tindao", "from controller with model");
+    // return "hello"; // controller xử lí data và gửi lại cho view
+    // } // view render ra html
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello(); // controller gọi từ model (service) để tính toán hàm, dữ liệu
-        model.addAttribute("eric", test); // model sau khi có data rồi nó sẽ gửi lại thằng controller
+
+        model.addAttribute("eric", "test");
         model.addAttribute("tindao", "from controller with model");
-        return "hello"; // controller xử lí data và gửi lại cho view
-    } // view render ra html
+        return "hello";
+    }
 
     @RequestMapping("/admin/user") // get
     public String getUserPage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("newUser", new User() );
-        model.addAttribute("tindao", "from controller with model");
+        model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User Daotrungtin) {
         System.out.println("run here" + Daotrungtin);
+        // lưu vào database lun
+        this.userService.handleSaveUser(Daotrungtin);
         return "hello";
     }
 }

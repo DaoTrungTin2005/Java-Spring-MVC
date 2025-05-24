5296eac0076a198e72021d96fc03c29f8345208d
Tín <Trungtin0701200@gmail.com>
2025-05-24 17:45:15 +0700

    one to many

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,13 +1,18 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +25,15 @@ public class User {
     private String avatar;
 
     // roleId
+
+    // User many -> to one -> role
+    @ManyToOne
+    // tạo cột role_id trong bảng user, dùng làm khóa ngoại tham chiếu đến bảng role
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
 
     public void setId(long id) {
         this.id = id;
@@ -82,8 +96,5 @@ public class User {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullname=" + fullname
                 + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
-
-
-
 
 }

c7fcaccb18798114850ce2cf163cb0f7ff45c215
Tín <Trungtin0701200@gmail.com>
2025-06-02 00:42:11 +0700

    Authorize by Role

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import jakarta.servlet.DispatcherType;
 import vn.hoidanit.laptopshop.service.CustomUserDetailsService;
@@ -58,28 +59,44 @@ public class SecurityConfiguration {
         return authProvider;
     }
 
+
+    @Bean
+    public AuthenticationSuccessHandler customSuccessHandler(){
+        return new CustomSuccessHandler();
+    }
+
+
+
     // dòng code này trang login của bản thân sẽ hiện ra thay cho trang login của
     // spring security
     // sẽ có lỗi redirect too many
     // gắn thêm máy dòng thì dô lại dc
-    // thêm "/" để khi dô trang chủ nó ko đá mình về login  (tragn chủ ko cần đăng nhập nữa)
+    // thêm "/" để khi dô trang chủ nó ko đá mình về login (tragn chủ ko cần đăng
+    // nhập nữa)
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-        // DispatcherType.FORWARD cho phép truy cập tới view
-        // DispatcherType.INCLUDE  ko có cái này dô trang chủ nó ko load dc
+                // DispatcherType.FORWARD cho phép truy cập tới view
+                // DispatcherType.INCLUDE ko có cái này dô trang chủ nó ko load dc
+
+                // có product hì khi nhấn xem chi tiết sản phẩm ko bị đá vò trng login
                 .dispatcherTypeMatchers(DispatcherType.FORWARD,
                         DispatcherType.INCLUDE)
                 .permitAll()
-                .requestMatchers("/","/login", "/client/**", "/css/**", "/js/**",
+                .requestMatchers("/", "/login", "/product/**", "/register", "/client/**", "/css/**", "/js/**",
                         "/images/**")
                 .permitAll()
+
+                // giao diện admin chỉ có admin mới dc dô
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+
                 .anyRequest().authenticated())
 
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
                         // khi login failed nó dẫn tới link này
                         .failureUrl("/login?error")
+                        .successHandler(customSuccessHandler())
                         .permitAll());
         return http.build();
     }
--- src/main/java/vn/hoidanit/laptopshop/service/CustomUserDetailsService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/CustomUserDetailsService.java
@@ -9,36 +9,34 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-
-
 // Lấy thông tin người dùng từ database khi người dùng đăng nhập.
 
-
 @Service
-public class CustomUserDetailsService implements UserDetailsService{
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserService userService;
 
-    public CustomUserDetailsService (UserService userService){
-        this.userService = userService ;
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
     }
-
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-            // viết logic
-            // 2 cái  User (1 cái cảu hoidanit 1 cái của spring )
-            vn.hoidanit.laptopshop.domain.User  user =this.userService.getUserByEmail(username) ;
-            if (user == null){
-                throw new UsernameNotFoundException("user ko tồn tại") ;
+        // viết logic
+        // 2 cái User (1 cái cảu hoidanit 1 cái của spring )
+        vn.hoidanit.laptopshop.domain.User user = this.userService.getUserByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("user ko tồn tại");
 
-            }
-// đa hinh : thằng User nó kế thừa UserDetail nên ko cần trả lại kiểu thằng cha mà trả kiểu thằng con là đươc
-// thằng java tự ddộng ép kiểu
-           return new User(
+        }
+        // đa hinh : thằng User nó kế thừa UserDetail nên ko cần trả lại kiểu thằng cha
+        // mà trả kiểu thằng con là đươc
+        // thằng java tự ddộng ép kiểu
+        return new User(
                 user.getEmail(),
                 user.getPassword(),
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))); 
-                }
+                // role là 1 đối tượng
+                // lấy động role theo database
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().getName())));
     }
-    
+}
